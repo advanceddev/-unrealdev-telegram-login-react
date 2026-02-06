c0410ef5a24ef75520a44c87aa555c00def72f2f
advanceddev <advanced.develope@gmail.com>
2026-02-06 20:16:19 +0500

    hotfix: build ES only

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,9 @@ export default defineConfig({
     host: "127.0.0.1",
     port: 80,
   },
-  plugins: [react(), Sonda()],
+  plugins: [react({
+    jsxRuntime: 'classic'
+  }), Sonda()],
   build: {
     // sourcemap: true, // for Sonda() using only
     lib: isDemo
@@ -18,12 +20,8 @@ export default defineConfig({
       : {
         entry: resolve(__dirname, 'src/index.ts'),
         name: 'TelegramLoginReact',
-        formats: ['es', 'cjs'],
-        fileName: (format) => {
-          if (format === 'es') return 'telegram-login-react.es.js';
-          if (format === 'cjs') return 'telegram-login-react.cjs';
-          return 'telegram-login-react.js';
-        },
+        formats: ['es'],
+        fileName: () => 'index.js',
       },
     rolldownOptions: isDemo
       ? {}
